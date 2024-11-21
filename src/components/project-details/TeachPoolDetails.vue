97f53be6a2678d5be61b850ebd501f115dbdff4c
Johannes Markwordt <johannesmarkwordt@hotmail.com>
2024-11-21 13:39:40 +0100

    feat: add TeachPool Page

--- src/components/project-details/TeachPoolDetails.vue
+++ src/components/project-details/TeachPoolDetails.vue
@@ -2,23 +2,53 @@
   <div class="space-y-6 w-full items-center">
     <img src="/teachpool.png" alt="Little Creek Detail" class="w-1/2 h-auto rounded-lg mx-auto" />
     <div class="prose prose-invert">
-      <h3 class="text-lime-500 text-center text-2xl font-bold">
-        TeachPool - Be the teacher!
-      </h3>
+      <h3 class="text-lime-500 text-center text-2xl font-bold">TeachPool - Be the teacher!</h3>
 
       <p class="text-center py-4 leading-relaxed text-lg">
-        
+        Im Rahmen unseres Fachabiturs haben wir das Projekt TeachPool entwickelt, eine App, die
+        den Prozess der Nachhilfevermittlung effizienter gestalten sollte. Dabei haben wir uns
+        bewusst für eine Herausforderung entschieden, indem wir auf React Native setzten, anstatt
+        das aus dem Unterricht bekannte Android Studio mit Java zu verwenden. Dies ermöglichte uns,
+        mit einem modernen Tech-Stack zu arbeiten, der plattformübergreifende Entwicklung
+        unterstützt. Für die Backend-Infrastruktur haben wir Firebase genutzt, das sowohl die
+        Authentifizierung als auch die datenbasierte Speicherung übernahm. Zudem wurde Expo
+        verwendet, um das Debugging und die Tests auf verschiedenen Geräten zu vereinfachen.
+      </p>
+      <p class="text-center py-4 leading-relaxed text-lg">
+        Die App verfügte über umfangreiche Funktionen, wie etwa eine Login- und Registrierungsoption
+        mit automatischem Mailversand sowie die Möglichkeit, persönliche Profildaten zu verwalten.
+        Nutzer konnten sich als Nachhilfelehrer freischalten lassen, indem sie relevante Nachweise
+        wie Zeugnisse hochluden, die im Rahmen eines Authentifizierungsprozesses geprüft wurden.
+        Eine integrierte Suchfunktion erlaubte es, Lehrer nach bis zu zwei Fachgebieten zu filtern
+        und einen Suchradius einzustellen. Hierbei wurde mithilfe einer Karten-API die Luftlinie
+        zwischen den angegebenen Adressen berechnet, um passende Lehrer im Umkreis anzuzeigen. Je
+        nach Status der Nutzer generierte die App zudem unterschiedliche Navigationsoptionen,
+        wodurch eine dynamische Benutzererfahrung gewährleistet wurde.
+      </p>
+      <p class="text-center py-4 leading-relaxed text-lg">
+        Das Projekt wurde in einer dreiköpfigen Gruppe durchgeführt, wobei der Großteil der
+        Entwicklung – etwa 90 Prozent – von mir übernommen wurde. Dies lag daran, dass ein
+        Gruppenmitglied krankheitsbedingt ausfiel und ein weiteres Mitglied aufgrund technischer
+        Probleme nicht mehr aktiv am Programmieren teilnehmen konnte. Stattdessen übernahm dieses
+        Mitglied die Planung und Dokumentation der App. TeachPool war ein herausforderndes, aber
+        erfolgreiches Projekt, das uns erlaubte, moderne Technologien praxisnah zu nutzen und unser
+        Wissen deutlich zu erweitern.
+      </p>
+      <p class="text-center py-4 leading-relaxed text-lg">
+        Das Projekt wurde mit der Note 1,0 bewertet. Im Anschluss schlug der begleitende Lehrer vor,
+        das Projekt in Form einer Präsentation im Partnerunternehmen vorzustellen, das die Klasse
+        während des Abschlussprojekts unterstützt hatte. Die Vorstellung fand im Rahmen einer
+        wöchentlichen Besprechung statt, an der alle Mitarbeitenden des Unternehmens teilnahmen.
+        Dies bot die Möglichkeit, das Projekt und dessen technische Umsetzung vor einem fachkundigen
+        Publikum zu präsentieren und Rückmeldungen aus der Praxis zu erhalten.
       </p>
 
       <h3 class="text-lime-500 text-center text-2xl font-bold">Hauptaufgaben</h3>
       <ul class="list-disc list-inside text-lg text-center py-4">
-        <li>Konzeptierung und Entwicklung von Systemen in Typescript</li>
+        <li>Entwicklung der App</li>
+        <li>Erstellung notwendiger Diagramme für die Dokumentation</li>
+        <li>Mockup Erstellung der einzelnen Screens</li>
         <li>Projektplanung</li>
-        <li>Server-Infrastruktur verwaltet und aufgebaut</li>
-        <li>Allgemeine Entwicklungsleitung</li>
-        <li>Projektleitung</li>
-        <li>Game Design</li>
-        <li>Grafikdesign</li>
       </ul>
     </div>
   </div>
